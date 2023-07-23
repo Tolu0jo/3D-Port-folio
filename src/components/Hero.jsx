47f06ba1b3424e33572681ad8faf2bfaf2cfba87
ToluOjo <ojotolulpe137@gmail.com>
2023-07-23 23:06:38 +0100

    add computer canvas

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,8 +5,9 @@ import { ComputersCanvas } from './canvas'
 
 const Hero = () => {
   return (
+    <>
     <section className='relative w-full h-screen mx-auto'>
-      <div className={`${styles.paddingY} absolute inset-0
+      <div className={`${styles.paddingX} absolute inset-0
     top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5}`}>
       <div className='flex flex-col justify-center items-center mt-5'>
         <div className='w-5 h-5 rounded-full bg-[#915eff]'/>
@@ -16,12 +17,33 @@ const Hero = () => {
        <div>
         <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#915eff]'>Tolulope</span></h1>
         <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-          I develop professional and scalable user <br className='sm:block hidden'/>interfaces, mobile and web applications.
+          I develop professional and scalable user <br className='sm:block hidden'/>
+          interfaces, mobile and web applications.
         </p>
        </div>
        </div>
-       <ComputersCanvas/>
+          <ComputersCanvas/>
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href='#about'>
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary
+          flex justify-center items-start p-2">
+            <motion.dev
+            animate={{
+              y: [0, 24, 0]
+            }}
+            transition={{
+              duration:2.0,
+              repeat: Infinity,
+              repeatType: "loop",
+            }} 
+            className="w-3 h-3 rounded-full bg-secondary mb-1"/>
+          </div>
+        </a>
+
+      </div>
     </section>
+ 
+    </>
   )
 }
 
